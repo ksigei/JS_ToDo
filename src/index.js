4e6368e7f2800ae918876d312efdc3478f79c5a0
ksigei <sigeikiprono4@gmail.com>
2022-03-29 21:27:45 +0300

    Linter errors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,73 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, imported
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('.todo-input');
+const todoItemsList = document.querySelector('.todo-items');
+let todos = [];
+todoForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addTodo(todoInput.value);
+});
+function addTodo(item) {
+  if (item !== '') {
+    const todo = {
+      id: Date.now(),
+      name: item,
+      completed: false,
+    };
+    todos.push(todo);
+    addToLocalStorage(todos);
+    todoInput.value = '';
+  }
+}
+function renderTodos(todos) {
+  todoItemsList.innerHTML = '';
+  todos.forEach((item) => {
+    const checked = item.completed ? 'checked' : null;
+    const li = document.createElement('li');
+    li.setAttribute('class', 'item');
+    li.setAttribute('data-key', item.id);
+    if (item.completed === true) {
+      li.classList.add('checked');
+    }
+    li.innerHTML = `
+      <input type="checkbox" class="checkbox" ${checked}>
+      ${item.name}
+      <button class="delete-button">X</button>
+    `;
+    todoItemsList.append(li);
+  });
+}
+function addToLocalStorage(todos) {
+  localStorage.setItem('todos', JSON.stringify(todos));
+  renderTodos(todos);
+}
+function getFromLocalStorage() {
+  const reference = localStorage.getItem('todos');
+  if (reference) {
+    todos = JSON.parse(reference);
+    renderTodos(todos);
+  }
+}
+function toggle(id) {
+  todos.forEach((item) => {
+    if (item.id !== id) {
+      item.completed = !item.completed;
+    }
+  });
+  addToLocalStorage(todos);
+}
+function deleteTodo(id) {
+  todos = todos.filter((item) => item.id != id);
+  addToLocalStorage(todos);
 }
 
-document.body.appendChild(component());
+getFromLocalStorage();
+todoItemsList.addEventListener('click', (event) => {
+  if (event.target.type === 'checkbox') {
+    toggle(event.target.parentElement.getAttribute('data-key'));
+  }
+  if (event.target.classList.contains('delete-button')) {
+    deleteTodo(event.target.parentElement.getAttribute('data-key'));
+  }
+});
