a673ed3ba3a7da2d7224a8eca17200bb6230dc25
ksigei <sigeikiprono4@gmail.com>
2022-03-30 11:40:30 +0300

    Render tasks from js

--- src/index.js
+++ src/index.js
@@ -1,59 +1,34 @@
 import './style.css';
 
-const todoForm = document.querySelector('.todo-form');
-const todoInput = document.querySelector('.todo-input');
 const todoItemsList = document.querySelector('.todo-items');
-let todos = [];
 
-function renderTodos() {
-}
-function addToLocalStorage(todos) {
-  localStorage.setItem('todos', JSON.stringify(todos));
-  renderTodos(todos);
-}
-function getFromLocalStorage() {
-  const reference = localStorage.getItem('todos');
-  if (reference) {
-    todos = JSON.parse(reference);
-    renderTodos(todos);
-  }
-}
-function toggle(id) {
-  todos.forEach((item) => {
-    if (item.id !== id) {
-      item.completed = !item.completed;
-    }
-  });
-  addToLocalStorage(todos);
-}
-function deleteTodo(id) {
-  todos = todos.filter((item) => item.id !== id);
-  addToLocalStorage(todos);
-}
+const todo = [
+  {
+    id: Date.now(),
+    name: 'Read a book',
+    completed: false,
+  },
+  {
+    id: Date.now(),
+    name: 'Some JS',
+    completed: false,
+  },
+];
 
-getFromLocalStorage();
-todoItemsList.addEventListener('click', (event) => {
-  if (event.target.type === 'checkbox') {
-    toggle(event.target.parentElement.getAttribute('data-key'));
-  }
-  if (event.target.classList.contains('delete-button')) {
-    deleteTodo(event.target.parentElement.getAttribute('data-key'));
-  }
-});
+todoItemsList.innerHTML = '';
+todo.forEach((item) => {
+  const checked = item.completed ? 'checked' : null;
+  const li = document.createElement('li');
+  li.setAttribute('class', 'item');
+  li.setAttribute('data-key', item.id);
 
-function addTodo(item) {
-  if (item !== '') {
-    const todo = {
-      id: Date.now(),
-      name: item,
-      completed: false,
-    };
-    todos.push(todo);
-    addToLocalStorage(todos);
-    todoInput.value = '';
+  if (item.completed === true) {
+    li.classList.add('checked');
   }
-}
-todoForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  addTodo(todoInput.value);
+  li.innerHTML = `
+        <input type="checkbox" class="checkbox" ${checked}>
+        ${item.name}
+        <button class="delete-button">X</button>
+      `;
+  todoItemsList.append(li);
 });
