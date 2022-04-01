ce1341edbfb018ad8a65463aeaf3385146123259
ksigei <sigeikiprono4@gmail.com>
2022-04-01 09:19:47 +0300

    Use array of objects, added tasks can be edited

--- src/index.js
+++ src/index.js
@@ -1,14 +1,65 @@
 import './style.css';
 
+import { saveLocalStorage, updateCompleted } from './modules/local_storage.js';
 import {
-  addTodo,
-  getTodoFromLocalStorage,
-  checkOrDelete,
-} from './modules/local_storage.js';
+  editTodo, removeTodos, addTodo, clearCompletedTodos,
+} from './modules/new_todo.js';
 
-const addTodoButton = document.querySelector('.add-todo-button');
-const todoList = document.querySelector('.todo-list');
+let itemsArray = [];
 
-document.addEventListener('DOMContentLoaded', getTodoFromLocalStorage);
-addTodoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', checkOrDelete);
+const storageData = JSON.parse(localStorage.getItem('toDoStorage'));
+if (storageData) {
+  itemsArray = storageData;
+}
+
+const todos = document.querySelector('.todos');
+itemsArray.forEach((item) => {
+  const html = `<input type="checkbox">
+  <input class="edit" value=${item.description} type="text"></input>
+  <i class="fas fa-ellipsis-v vertical"></i>
+  <i class="fas fa-trash-alt trash"></i>`;
+  const div = document.createElement('div');
+  div.innerHTML = html;
+  const input = div.querySelector('input[type=checkbox]');
+  input.checked = item.completed;
+  div.classList.add('todo-item');
+  div.id = item.index;
+
+  input.addEventListener('change', () => {
+    updateCompleted(item, input);
+    saveLocalStorage(itemsArray);
+  });
+
+  todos.appendChild(div);
+  const vertical = div.querySelector('.vertical');
+  const trash = div.querySelector('.trash');
+  const edit = div.querySelector('.edit');
+  edit.addEventListener('change', () => {
+    editTodo(itemsArray, div.id, edit.value);
+    saveLocalStorage(itemsArray);
+  });
+  vertical.addEventListener('click', () => {
+    vertical.style.display = 'none';
+    trash.style.display = 'block';
+  });
+  trash.addEventListener('click', () => {
+    div.remove();
+    itemsArray = itemsArray.filter((i) => removeTodos(i, div.id));
+    saveLocalStorage(itemsArray);
+  });
+});
+
+const form = document.getElementById('form-id');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const { value } = form.querySelector('input');
+  addTodo(itemsArray, value);
+  saveLocalStorage(itemsArray);
+  window.location.reload();
+});
+
+const clearButton = document.querySelector('#button');
+clearButton.addEventListener('click', () => {
+  itemsArray = clearCompletedTodos(itemsArray);
+  saveLocalStorage(itemsArray);
+});
--- src/modules/new_todo.js
+++ src/modules/new_todo.js
@@ -1,25 +1,36 @@
-const todoList = document.querySelector('.todo-list');
-
-const createElements = (value) => {
-  const todoDiv = document.createElement('div');
-  todoDiv.classList.add('todo');
-
-  const newTodo = document.createElement('li');
-  newTodo.innerText = value;
-  newTodo.classList.add('todo-item');
-
-  todoDiv.appendChild(newTodo);
-
-  const checkButton = document.createElement('button');
-  checkButton.classList.add('fas', 'fa-check', 'check-btn'); // error if use space, so split class with '',
-
-  todoDiv.appendChild(checkButton);
-
-  const deleteButton = document.createElement('button');
-  deleteButton.classList.add('fas', 'fa-trash', 'delete-btn');
-  todoDiv.appendChild(deleteButton);
-
-  todoList.appendChild(todoDiv);
+export const editTodo = (itemsArray, id, newValue) => {
+  itemsArray.forEach((item) => {
+    if (item.index.toString() === id) {
+      item.description = newValue;
+    }
+  });
 };
 
-export default createElements;
+export const removeTodos = (something, id) => {
+  if (something.index.toString() === id) {
+    return false;
+  }
+  return true;
+};
+
+export const addTodo = (itemsArray, value) => {
+  itemsArray.push({ completed: false, description: value, index: itemsArray.length + 1 });
+};
+
+export const clearCompletedTodos = (array) => {
+  let resultArray = array;
+  const todoArray = document.querySelectorAll('.todo-item');
+  todoArray.forEach((div) => {
+    const input = div.querySelector('input');
+    if (input.checked) {
+      div.remove();
+      resultArray = array.filter((something) => {
+        if (something.index.toString() === div.id) {
+          return false;
+        }
+        return true;
+      });
+    }
+  });
+  return resultArray;
+};
